bb46bbe9e0a679ec570f77d7e71d86fe520192e7
Binil Starly <bstarly@asurite.asu.edu>
2024-05-09 13:16:27 -0700

    third

---
+++ controls.py
@@ -0,0 +1,15 @@
+SERVICE_TYPES = [
+     'Casting',
+     'Molding',
+     'Moulding',
+     'Joining',
+     'Additive Manufacturing',
+     'CMM Inspection',
+     'Reverse Engineering',
+     'Machining',
+     'Custom Injection Molding',
+     'Job Shop',
+     'Forging',
+     'Forming',
+     'Unknown'
+]
